f5aed20d1594425dafc78fc7dc9a2fc9707a9bdd
Fernando Soto Lavayen <Fernando.Soto@fundacion-jala.org>
2018-11-18 19:16:29 -0400

    scenes created

---
+++ Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSelector : MonoBehaviour {
+
+    public void GoToRules()
+    {
+        this.SendToScene("Rules");
+    }
+    public void GoToLevels()
+    {
+        this.SendToScene("Levels");
+    }
+
+    public void GoToGame()
+    {
+        this.SendToScene("Game");
+    }
+
+    public void GoToMenu()
+    {
+        this.SendToScene("Menu");
+    }
+
+    private void SendToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
